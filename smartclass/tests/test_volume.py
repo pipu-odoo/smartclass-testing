e1e528d84dbcbdf1a95e313dec4f82767d0e7590
Pierre Pulinckx <pipu@odoo.com>
2025-09-11 08:43:54 +0200

    unit test python

---
+++ smartclass/tests/test_volume.py
@@ -0,0 +1,33 @@
+# ./odoo-bin --config=../.vscode/odoo.conf -u smartclass --test-enable --test-tags=:TestVolume
+
+import odoo.tests
+
+
+@odoo.tests.tagged('post_install', '-at_install')
+class TestVolume(odoo.tests.HttpCase):
+
+    def test_volume_model(self):
+        volume0 = self.env["smartclass.volume"].create({
+            "name": "volume 0",
+            "width": 2.56,
+            "height": 1.34,
+        })
+        self.assertEqual(volume0.volume, 0)
+        self.assertEqual(volume0.category, "small")
+        volume1 = self.env["smartclass.volume"].create({
+            "name": "volume 1",
+            "depth": 1,
+            "width": 1,
+            "height": 0.2,
+        })
+        self.assertEqual(volume1.volume, 0.2)
+        self.assertEqual(volume1.category, "small")
+        volume2 = self.env["smartclass.volume"].create({
+            "name": "volume 2",
+            "depth": 7.24,
+            "width": 2.56,
+            "height": 1.34,
+        })
+        self.assertAlmostEqual(volume2.volume, 24.84, places=2)
+        self.assertEqual(volume2.category, "medium")
+
